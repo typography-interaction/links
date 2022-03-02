719d54615e7762d21c3fb135846bcba2afb73c9f
Michael Fehrenbach <mfehrenbach@gmail.com>
2022-03-01 22:03:30 -0500

    Set the title, as a start.

--- assets/arena.js
+++ assets/arena.js
@@ -1,6 +1,16 @@
-let channel = 'typography-and-interaction-too'
-let url = `https://api.are.na/v2/channels/${channel}`
+const channel = 'typography-and-interaction-too'
+const url = `https://api.are.na/v2/channels/${channel}`
+
+
+
+const constructElements = (data) => {
+	document.title = data.title;
+}
+
+
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
+	.then(response => response.json())
+	.then(data => {
+		constructElements(data)
+	})
